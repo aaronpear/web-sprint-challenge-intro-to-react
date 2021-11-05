10823e6755054c919019dc7b4f29a6873cefb655
Aaron Reyes <56135909+aaronpear@users.noreply.github.com>
2021-11-05 11:28:46 -0700

    MVP Technically Complete, looks horrible though lol

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,15 @@
+import React, { useState } from "react";
+
 // Write your Character component here
+
+const Character = (props) => {
+    const { name } = props;
+
+    return (
+        <p>
+        {name}
+        </p>
+    );
+}
+
+export default Character;
